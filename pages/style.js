fa0b5d0a5792fa91465422c5b8886232ec55bee7
qiru <bjqiru@corp.netease.com>
2017-08-29 19:40:05 +0800

    nextjs样式

---
+++ pages/style.js
@@ -0,0 +1,71 @@
+import Layout from '../components/Layout.js'
+import Link from 'next/link'
+
+function getPosts () {
+   return [
+      { id: 'hello-nextjs', title: 'Hello Next.js'},
+      { id: 'learn-nextjs', title: 'Learn Next.js is awesome'},
+      { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT'},
+   ]
+}
+
+
+const PostLink = ({ post }) => (
+  <li>
+    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+      <a>{post.title}</a>
+    </Link>
+    {/*<style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+   `}</style>*/}
+  </li>
+  
+)
+
+export default () => (
+   <Layout>
+      <h1>My Blog</h1>
+      <ul>
+         {getPosts().map((post) => (
+            <PostLink key={post.id} post={post}/>
+         ))}
+      </ul>
+      <style jsx>{`
+         h1, a {
+           font-family: "Arial";
+         }
+
+         ul {
+           padding: 0;
+         }
+      `}</style>
+      {/*使用全局样式就可以作用与其他组件*/}
+      <style jsx global>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+   `}</style>
+  </Layout>
+)
